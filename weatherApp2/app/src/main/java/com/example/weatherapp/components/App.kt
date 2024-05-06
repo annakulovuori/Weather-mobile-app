068d360632662f2d69734c5528915f092464e6ee
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-06 16:40:17 +0300

    Create: NavGraphBuilder funktion.

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/App.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/App.kt
@@ -1,13 +1,19 @@
 package com.example.weatherapp.components
 
+import android.annotation.SuppressLint
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.weatherapp.components.ForecastScreen
 import com.example.weatherapp.components.WeatherNowScreen
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun App() {
     // Create a NavController to handle navigation between composables
@@ -15,9 +21,13 @@ fun App() {
     Scaffold (
         bottomBar = {}
     ){
-
+        NavGraphBuilder(navController = navController)
     }
 
+
+}
+@Composable
+fun NavGraphBuilder(navController: NavHostController) {
     // Define a navigation host that controls the navigation within this composable
     NavHost(navController = navController, startDestination = "WeatherNowScreen") {
         composable(BottomBar.Home.route) {
@@ -27,5 +37,4 @@ fun App() {
             ForecastScreen()
         }
     }
-
 }
