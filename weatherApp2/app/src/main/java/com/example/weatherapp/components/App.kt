44c35cdd68d3643bb9aa7995b7eb17c1832709e8
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-06 17:11:38 +0300

    Create: NavGraphBuilder function, CreateBottomBar function, AddItem function

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/App.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/App.kt
@@ -1,14 +1,23 @@
 package com.example.weatherapp.components
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Text
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.weatherapp.components.ForecastScreen
 import com.example.weatherapp.components.WeatherNowScreen
@@ -19,7 +28,7 @@ fun App() {
     // Create a NavController to handle navigation between composables
     val navController = rememberNavController()
     Scaffold (
-        bottomBar = {}
+        bottomBar = { CreateBottomBar(navController = navController)}
     ){
         NavGraphBuilder(navController = navController)
     }
@@ -37,4 +46,46 @@ fun NavGraphBuilder(navController: NavHostController) {
             ForecastScreen()
         }
     }
+}
+
+@Composable
+fun CreateBottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBar.Home,
+        BottomBar.Forecast
+    )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    BottomNavigation (){
+        screens.forEach { screen ->
+            AddItem(screen = screen, currentDestination = currentDestination, navController = navController)
+        }
+    }
+}
+
+@Composable
+fun RowScope.AddItem(
+    screen: BottomBar,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    BottomNavigationItem(
+        label = {
+                Text(screen.title)
+        },
+        icon = {
+            Icon(
+                imageVector = screen.icon,
+                contentDescription = "Navigation icon"
+            )
+        },
+        selected = currentDestination?.hierarchy?.any {
+            it.route == screen.route
+        } == true,
+        onClick = {
+            navController.navigate(screen.route)
+        }
+    )
 }
