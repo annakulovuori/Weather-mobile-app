5e1009cb256f5f40882b1d03daa59f9703751030
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-06 16:36:20 +0300

    Refactor: NavHost composables

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/App.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/App.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapp.components
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -11,14 +12,19 @@ import com.example.weatherapp.components.WeatherNowScreen
 fun App() {
     // Create a NavController to handle navigation between composables
     val navController = rememberNavController()
+    Scaffold (
+        bottomBar = {}
+    ){
+
+    }
 
     // Define a navigation host that controls the navigation within this composable
     NavHost(navController = navController, startDestination = "WeatherNowScreen") {
-        composable("WeatherNowScreen") {
-            WeatherNowScreen(navController)
+        composable(BottomBar.Home.route) {
+            WeatherNowScreen()
         }
-        composable("ForecastScreen") {
-            ForecastScreen(navController)
+        composable(BottomBar.Forecast.route) {
+            ForecastScreen()
         }
     }
 
