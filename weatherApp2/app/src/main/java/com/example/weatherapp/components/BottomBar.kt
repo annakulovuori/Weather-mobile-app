f6752bb1f13e51383ef97bec0ccda9e38a95f7b9
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-06 16:23:18 +0300

    Create: BottomBar.kt, sealed class BottomBar, Objects Home and Forecast

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/BottomBar.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/BottomBar.kt
@@ -1,4 +1,24 @@
 package com.example.weatherapp.components
 
-class BottomBar {
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomBar (
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+) {
+    object Home : BottomBar (
+        route = "WeatherNowScreen",
+        title = "Home",
+        icon = Icons.Default.Home
+    )
+    object Forecast : BottomBar (
+        route = "ForecastScreen",
+        title = "week",
+        icon = Icons.Default.DateRange
+    )
+
 }
