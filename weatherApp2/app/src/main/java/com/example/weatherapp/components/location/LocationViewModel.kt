6010d5f66ba52c09b003b83f4fab25d642aed54c
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-08 18:21:24 +0300

    Create: LocationViewModel to start updating location

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/location/LocationViewModel.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/location/LocationViewModel.kt
@@ -1,4 +1,21 @@
 package com.example.weatherapp.components.location
 
-class LocationViewModel {
+import android.app.Application
+import android.location.Location
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
+
+
+class LocationViewModel(application: Application) : AndroidViewModel(application) {
+    private val locationRepository = LocationRepository(application)
+
+    private val _location = mutableStateOf<Location?>(null)
+    val location: State<Location?> = _location
+
+    fun startLocationUpdates() {
+        locationRepository.startLocationUpdates { location ->
+            _location.value = location
+        }
+    }
 }
