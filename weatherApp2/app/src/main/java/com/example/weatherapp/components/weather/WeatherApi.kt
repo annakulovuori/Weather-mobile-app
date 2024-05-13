720afe2875b38359c1549eeef058cebc988cc7c3
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-13 16:33:43 +0300

    Create: weatherApi interface, getWeatherData suspend function, companion object with base url

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/weather/WeatherApi.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/weather/WeatherApi.kt
@@ -1,4 +1,17 @@
 package com.example.weatherapp.components.weather
 
+import com.example.weatherapp.components.location.locationOutput
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface WeatherApi {
+    @GET
+    suspend fun getWeatherData(
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double
+    ): Weather
+
+    companion object {
+        const val BASE_URL = "https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&wind_speed_unit=ms&timezone=auto"
+    }
 }
