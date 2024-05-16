b36e2991d69b5ef0c960d3243dc7eb2215220aa5
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-16 20:42:16 +0300

    Refactor: LocationOutPut file name to WeatherInfo. Create: weatherInfo function, trying to pass latitude and longitude to WeatherRepositorys getWeatherList unsuccessfully

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/location/WeatherInfo.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/location/WeatherInfo.kt
@@ -4,17 +4,16 @@ import android.Manifest
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.core.location.component1
 import androidx.core.location.component2
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.weatherapp.components.weather.WeatherApi
+import com.example.weatherapp.components.weather.WeatherApiService
 import com.example.weatherapp.components.weather.WeatherRepository
 
 @Composable
-fun WeatherScreen() {
+fun weatherInfo() {
     val viewModel: LocationViewModel = viewModel()
     val location = viewModel.location
 
@@ -41,10 +40,11 @@ fun WeatherScreen() {
 
     if (locationData != null) {
         val (latitude, longitude) = locationData
-        val weatherList by WeatherRepository.getWeatherList(latitude, longitude).collectAsState(initial = emptyList())
+        val weatherRepository = WeatherRepository(WeatherApiService.create())
+        val weatherList by weatherRepository.getWeatherList(latitude, longitude).collectAsState(initial = emptyList())
 
-        // Render your UI with weatherList
+
     } else {
-        // Render loading or error state
+        // Laita loading thai error
     }
 }
