a0ed7aacca06408c4a471e16535cd7c12231142c
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-16 20:44:44 +0300

    Create: getWeatherList function with parameters latitude and longitude. Feat: error handling with try catch

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/weather/WeatherRepository.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/weather/WeatherRepository.kt
@@ -1,4 +1,26 @@
 package com.example.weatherapp.components.weather
 
-class WeatherRepository {
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+
+
+class WeatherRepository(
+    private val api: WeatherApi
+) {
+    suspend fun getWeatherList(latitude: Double, longitude: Double): Flow<Result<List<Weather>>> {
+        return flow {
+            val weatherFromApi = try {
+                api.getWeatherData(latitude = latitude, longitude = longitude)
+            } catch (e: IOException) {
+                emit(Result.Error(data = null, message = "Error loading weather"))
+                return@flow
+            } catch (e: HttpException) {
+                emit(Result.Error(data = null, message = "Error loading weather"))
+                return@flow
+            }
+        }
+    }
 }
