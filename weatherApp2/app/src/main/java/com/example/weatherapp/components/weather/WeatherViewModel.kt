d52b0ede1d45291a0a4b95d5946864c26d68c49d
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-22 20:14:38 +0300

    Create: WeatherViewModel class with weatherRepository variable and getTemperature function. Object WeatherViewModelFactory with create function. Class WeatherViewModelFactoryClass with weatherRepository variable, override create

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/weather/WeatherViewModel.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/weather/WeatherViewModel.kt
@@ -1,4 +1,50 @@
 package com.example.weatherapp.components.weather
 
-class WeatherViewModel {
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import androidx.lifecycle.ViewModelProvider
+
+class WeatherViewModel(
+    private val weatherRepository: WeatherRepository
+): ViewModel() {
+    // private val _temperature = MutableStateFlow<Double>()
+    private val _temperature = MutableStateFlow<Double?>(null) // StateFlow to hold weather data
+    val temperature = _temperature.asStateFlow() // Expose as StateFlow
+
+    fun getTemperature() {
+        viewModelScope.launch {
+            try {
+                val temperature = weatherRepository.getTemperature(23.2, 61.0) // Assuming this is a suspend function
+                _temperature.value = temperature
+            } catch (e: Exception) {
+                // Handle error if needed
+                println("Error fetching temperature: ${e.message}")
+            }
+        }
+    }
+}
+
+object WeatherViewModelFactory {
+    fun create(): WeatherViewModelFactoryClass {
+        val weatherApi = WeatherApiService.create()
+        val weatherRepository = WeatherRepository(weatherApi)
+        return WeatherViewModelFactoryClass(weatherRepository)
+    }
+}
+
+class WeatherViewModelFactoryClass(
+    private val weatherRepository: WeatherRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(WeatherViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return WeatherViewModel(weatherRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
