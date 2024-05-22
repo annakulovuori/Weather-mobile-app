51a5ac3460644b0991d1d116ff251a483d9c8ceb
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-22 20:16:14 +0300

    Feat: viewModel connection

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/WeatherNowScreen.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/WeatherNowScreen.kt
@@ -14,13 +14,23 @@ import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.weatherapp.components.weather.WeatherRepository
+import com.example.weatherapp.components.weather.WeatherViewModel
+import com.example.weatherapp.components.weather.WeatherViewModelFactory
+import kotlinx.coroutines.flow.StateFlow
 
 @Composable
 fun WeatherNowScreen() {
+    val viewModel: WeatherViewModel = viewModel(factory = WeatherViewModelFactory.create())
+    val temperature = viewModel.temperature.collectAsState().value
+
+    viewModel.getTemperature()
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -39,7 +49,7 @@ fun WeatherNowScreen() {
                         modifier = Modifier.size(48.dp)
                     )
                     Text(
-                        text = "Weather Now",
+                        text = "Weather Now: ${temperature ?: "Loading..."}",
                         modifier = Modifier.padding(start = 16.dp),
                         color = Color.White
                     )
