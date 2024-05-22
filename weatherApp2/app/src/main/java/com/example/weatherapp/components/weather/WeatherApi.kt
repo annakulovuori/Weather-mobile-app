03a07374481ccbf5c194db46e4869451245bffa5
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-22 20:08:45 +0300

    Feat: add forecast to get. Add current and timezone querys. Refactor: BASE_URL

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/weather/WeatherApi.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/weather/WeatherApi.kt
@@ -6,15 +6,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 interface WeatherApi {
-    @GET
+    @GET("forecast")
     suspend fun getWeatherData(
         @Query("latitude") latitude: Double,
-        @Query("longitude") longitude: Double
+        @Query("longitude") longitude: Double,
+        @Query("current") current: String = "temperature_2m",
+        @Query("timezone") timezone: String = "auto"
     ): Weather
 
 }
 object WeatherApiService {
-    private const val BASE_URL =  "https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&wind_speed_unit=ms&timezone=auto"
+    private const val BASE_URL = "https://api.open-meteo.com/v1/"
 
     fun create(): WeatherApi {
         val retrofit = Retrofit.Builder()
