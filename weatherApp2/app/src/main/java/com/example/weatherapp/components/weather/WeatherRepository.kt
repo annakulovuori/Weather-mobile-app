7f530acbe19d1dd5dce1a4753832f7e3953c6629
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-22 20:10:10 +0300

    Refactor: hard coded latitude and longitude in getWeatherData call

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/weather/WeatherRepository.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/weather/WeatherRepository.kt
@@ -9,8 +9,8 @@ class WeatherRepository(
 ) {
     suspend fun getTemperature(latitude: Double, longitude: Double): Double? {
         var temperatureFromApi: Double? = null
-            try {
-            temperatureFromApi = api.getWeatherData(latitude = latitude, longitude = longitude).current.temperature_2m
+        try {
+            temperatureFromApi = api.getWeatherData(latitude = 61.49773, longitude = 23.779099).current.temperature_2m
         } catch (e: IOException) {
             println("Error loading weather")
         } catch (e: HttpException) {
