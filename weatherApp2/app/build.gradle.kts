0fc20d1f7b18b8a6e091acc427444ab9af7de5c5
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-27 20:12:31 +0300

    tilpehööri

--- weatherApp2/app/build.gradle.kts
+++ weatherApp2/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.weatherapp"
         minSdk = 30
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
