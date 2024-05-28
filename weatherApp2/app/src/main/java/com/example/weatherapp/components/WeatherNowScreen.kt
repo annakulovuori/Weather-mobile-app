57fab6dbd39479556b1c611e9b9773d4c3e14041
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-28 13:22:44 +0300

    Feat: Use WeatherType class to fetch correct icon according to weathercode

--- weatherApp2/app/src/main/java/com/example/weatherapp/components/WeatherNowScreen.kt
+++ weatherApp2/app/src/main/java/com/example/weatherapp/components/WeatherNowScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.weatherapp.components.weather.Weather
+import com.example.weatherapp.components.weather.WeatherType
 import com.example.weatherapp.components.weather.WeatherViewModel
 import com.example.weatherapp.components.weather.WeatherViewModelFactory
 
@@ -32,11 +33,12 @@ fun WeatherNowScreen() {
     val scrollState = rememberScrollState()
 
     val currentTime = weather?.current?.time
-    println(currentTime)
     val timeList = weather?.hourly?.time
-    println(timeList)
+    println("Current Time: $currentTime")
+    println("Time List: $timeList")
+
     val timeIndexNow = getCurrentTimesIndex(weather = weather, weatherList = timeList, currentTime = currentTime)
-    println(timeIndexNow)
+    println("Time Index Now: $timeIndexNow")
 
     Box(
         modifier = Modifier
@@ -55,13 +57,14 @@ fun WeatherNowScreen() {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Spacer(modifier = Modifier.size(70.dp))
                 Icon(
-                    imageVector = Icons.Outlined.Cloud,
-                    contentDescription = "Cloud",
+                    imageVector = WeatherType.getWeatherType(weather?.current?.weather_code).icon,
+                    //imageVector = Icons.Outlined.Cloud,
+                    contentDescription = WeatherType.getWeatherType(weather?.current?.weather_code).weatherDesc,
                     modifier = Modifier.size(60.dp)
                 )
                 Spacer(modifier = Modifier.size(25.dp))
                 Text(
-                    text = "${weather?.current?.temperature_2m ?: "Loading..."}°C",
+                    text = "${weather?.current?.temperature_2m ?: "Loading"}°C",
                     fontSize = 55.sp,
                     modifier = Modifier.padding(start = 16.dp),
                     color = Color.White
@@ -70,7 +73,6 @@ fun WeatherNowScreen() {
             Spacer(modifier = Modifier.size(20.dp))
             Box(
                 modifier = Modifier
-                    //.background(color = Color(0xFF76B7DE), shape = RoundedCornerShape(16.dp))
                     .padding(30.dp)
                     .fillMaxWidth()
                     .heightIn(min = 0.dp, max = 400.dp)
@@ -95,13 +97,14 @@ fun WeatherNowScreen() {
                                     )
                                     Spacer(modifier = Modifier.size(50.dp))
                                     Icon(
-                                        imageVector = Icons.Outlined.WaterDrop,
-                                        contentDescription = "Sun",
+                                        imageVector = WeatherType.getWeatherType(weather.hourly.weather_code[hour]).icon,
+                                        //imageVector = Icons.Outlined.Cloud,
+                                        contentDescription = WeatherType.getWeatherType(weather.hourly.weather_code[hour]).weatherDesc,
                                         modifier = Modifier.size(30.dp)
                                     )
                                     Spacer(modifier = Modifier.size(15.dp))
                                     Text(
-                                        text = "${weather?.hourly?.temperature_2m?.get(hour) ?: "Loading..."}°C",
+                                        text = "${weather.hourly.temperature_2m[hour] ?: "Loading"}°C",
                                         fontSize = 30.sp,
                                         modifier = Modifier.padding(start = 16.dp),
                                         color = Color.White
