cea05ba95bf8f03d53fd50d3b78fbf20f32189ac
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-05-30 13:41:07 +0300

    Refactor: Version code and name for next release

--- weatherApp2/app/build.gradle.kts
+++ weatherApp2/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.weatherapp"
         minSdk = 30
         targetSdk = 34
-        versionCode = 3
-        versionName = "2.0.0"
+        versionCode = 4
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
